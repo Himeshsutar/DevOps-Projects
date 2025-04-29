a3973f423ac5f846aa36c842896ae8c10b951fa2
Himeshsutar <140190776+Himeshsutar@users.noreply.github.com>
2025-04-29 14:26:23 +0200

    Update settings.py
    Following best practices and removal of pass

--- Incident-Management-System/incident_management_system/settings.py
+++ Incident-Management-System/incident_management_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$qvdsx2dr$extq=phdn%y*rat&%@@xlg-es&daki0zfv$_=15y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'himeshsutarwork@gmail.com'       # 👉 your Gmail address
-EMAIL_HOST_PASSWORD = 'kobemcrzkdfoovou'      # 👉 App password (not your Gmail login password!)
+EMAIL_HOST_PASSWORD = ''      # 👉 App password (not your Gmail login password!)
